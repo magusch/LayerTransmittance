5547837d692e3a7217370b06b09e56bf7500e8eb
magusch <magusch@gmail.com>
2023-04-17 18:57:07 +0300

    Plot title

--- app/saving_plot.py
+++ app/saving_plot.py
@@ -140,9 +140,9 @@ class TransmittancePlotter:
             wv, output = self.het(d, ri)
 
         plt.ylabel(self.ylab[self.layer_params.y_label])
-        # str_title = title_to_str(parametrs['title'])
-        # plt.title(str_title)
-        plt.legend()  # title = parametrs['title']
+        str_title = self.title_to_str()
+        plt.title(str_title)
+        plt.legend()
         filename_plot = str(int(time.time())) + '.png'
         image_path = 'app/static/plot/' + filename_plot
         plt.savefig(image_path)
